a63a368093588d617cb631d1ea59fb9624884e7c
DoZ99 <doz@speedster.org.uk>
2014-12-08 17:57:19 +0000

    tidied up

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\bin\AndroidManifest.xml : \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\AndroidManifest.xml \
--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\gen\com\doz\xemap\lite\R.java \
- : C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\drawable\icon.png \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\drawable-hdpi\icon.png \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\drawable-ldpi\icon.png \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\drawable-mdpi\icon.png \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\drawable-xhdpi\icon.png \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\values\strings.xml \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\res\xml\config.xml \
-C:\DHdata\NetBeansProjects\XeMap\platforms\android\bin\AndroidManifest.xml \
